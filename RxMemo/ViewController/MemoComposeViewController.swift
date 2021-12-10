dbc2495d546db84ff07e1423265f0e5c6213f007
김영채 <kevinkim2586@gmail.com>
2021-12-10 14:09:38 +0900

    feat: implement bindViewModel in MemoComposeVC

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,20 +6,58 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
+import Action
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var contentTextView: UITextView!
+    
     
     var viewModel: MemoComposeViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+            
+        
     }
     
     func bindViewModel() {
         
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        // 메모 쓰기 모드에서는 빈문자열이 표시되고, 편지 모드에서는 편집할 메모가 표시된다
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler) // 더블탭을 막기 위해 0.5초에 한번씩만 tap 이벤트 처리
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
 
 }
