2edad485e987553d2ad4963dff0ac7b6419dcf5a
김영채 <kevinkim2586@gmail.com>
2021-12-11 15:54:42 +0900

    feat: bind tableView in MemoDetailVC

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -25,6 +25,26 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
     func bindViewModel() {
         
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.contents
+            .bind(to: listTableView.rx.items) { tableView, row, value in
+                switch row {
+                case 0:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                case 1:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
     }
 
 
--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+
+class MemoDetailViewModel: CommonViewModel {
+
+    let memo: Memo
     
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    /// 왜 BehaviorSubject인가?
+    /// 메모를 편집한 다음에 다시 보기화면으로 오면 편집한 내용이 반영되어야한다.
+    /// 이렇게 하기 위해서는 새로운 문자열 배열을 방출해야함
+    /// 일반 Observable 로 하면 이게 불간으함
+    var contents: BehaviorSubject<[String]>
+
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
+
+
 }
